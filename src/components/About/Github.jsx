70c9482fb5048328eac8ccbc7960fb0085b4112a
tamzidpeace <tamjedpeace@gmail.com>
2025-07-05 12:10:24 +0600

    refactor: Simplify colourTheme in Github component by removing unnecessary properties

--- src/components/About/Github.jsx
+++ src/components/About/Github.jsx
@@ -4,13 +4,7 @@ import { Row } from "react-bootstrap";
 
 function Github() {
   const colourTheme = {
-    background: "transparent",
-    text: "#ffffff",
-    grade4: "#8400b8",
-    grade3: "#b22ff4",
-    grade2: "#b265f6",
-    grade1: "#c084f5",
-    grade0: "#ecd9fc",
+    light: ["#ecd9fc", "#c084f5", "#b265f6", "#b22ff4", "#8400b8"],
   };
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
